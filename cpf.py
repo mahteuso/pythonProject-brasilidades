030da161f76e890d95dd8bb54208d741fd72675a
mateus <mateus@ufscar.br>
2023-03-28 06:27:59 -0300

    including external lib

--- cpf.py
+++ cpf.py
@@ -1,25 +1,29 @@
+from validate_docbr import CPF, CNH
+
+
 class Cpf:
-    def __init__(self, cpf):
+    def __init__(self, cpf, cnh):
         self.cpf = str(cpf)
+        self.cnh = str(cnh)
         if self.cpf_validates():
             self.cpf_format()
         else:
             raise ValueError('Invalid value, enter another number!')
 
     def __str__(self):
-        return self.cpf_format()
+        return f' Your cpf: {self.cpf_format()} and your cnh: {self.cnh_format()}'
 
     def cpf_validates(self):
-        if len(self.cpf) == 11:
-            return True
-        else:
-            return False
+        new_cpf = CPF()
+        return new_cpf.validate(self.cpf)
 
     def cpf_format(self):
-        cpf_part_one = self.cpf[:3]
-        cpf_part_two = self.cpf[3:6]
-        cpf_part_three = self.cpf[6:9]
-        cpf_part_four = self.cpf[9:]
-        return print(f'{cpf_part_one}.{cpf_part_two}.{cpf_part_three}-{cpf_part_four}')
+        cpf = CPF()
+        return cpf.mask(self.cpf)
 
-
+    def cnh_validate(self):
+        cnh = CNH()
+        return cnh.validate(self.cnh)
+    def cnh_format(self):
+        cnh = CNH()
+        return cnh.mask(self.cnh)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from cpf import Cpf
 
 cpf = 29838352861
+cnh = '04506588800'
 
-object_cpf = Cpf(cpf)
+object_cpf = Cpf(cpf, cnh)
+
+print(object_cpf)
 
 
 
