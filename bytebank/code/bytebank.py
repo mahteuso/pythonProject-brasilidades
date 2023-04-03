7a133eaa276e474108d2b6fda268a6fbc3475223
mateus <mateus@ufscar.br>
2023-04-03 12:28:35 -0300

    studying tests in python

--- bytebank/code/bytebank.py
+++ bytebank/code/bytebank.py
@@ -14,4 +14,21 @@ class Employee:
         return self._birth
     @property
     def wage(self):
-        return self.wage
+        return self._wage
+
+    def age(self):
+        date_birth = self.birth.split('/')
+        year_birth = date_birth[-1]
+        actual_year = date.today().year
+        return actual_year - int(year_birth)
+
+    def bonus_calculate(self):
+        value = self.wage * 0.1
+        if value > 1000:
+            value = 0
+        return value
+
+    def __str__(self):
+        return f'Employee: {self.name}\n' \
+               f'Age: {self.age()}\n' \
+               f'Wage: {self.wage}'
---
+++ bytebank/code/main.py
@@ -0,0 +1,5 @@
+from bytebank import Employee
+
+mateus = Employee('Mateus Laranjeira', '13/03/1970', 1000)
+
+print(mateus)
